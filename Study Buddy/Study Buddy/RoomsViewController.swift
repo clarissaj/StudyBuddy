32fca4e4f17f44fc1abbd8a4d8b1d0ec22bc3bcf
Clarissa Jawaid <clarissajawaid@gmail.com>
2020-01-05 20:34:47 -0600

    upgrade project to Swift 4.2 and add recommended xcode settings

--- Study Buddy/Study Buddy/RoomsViewController.swift
+++ Study Buddy/Study Buddy/RoomsViewController.swift
@@ -33,7 +33,7 @@ class RoomsViewController: UITableViewController {
         return cell
     }
     
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         
         if editingStyle == .delete {
             let room = rooms[indexPath.row]
